674268dc7a2f7406678446de03f839cf9732146c
Justin Walters <justinpw@gmail.com>
2023-08-07 10:31:24 -0400

    update for reports and alarms

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-import io, os, contextlib
+import io, os, contextlib, sys
 from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
-
 from pyvesync import VeSync
 
 DEVICE_NAME = 'Garage'
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
+VESYNC_EMAIL = os.environ.get('VESYNC_EMAIL')
+VESYNC_PASSWORD = os.environ.get('VESYNC_PASSWORD')
+SENDER = os.environ.get('GMAIL_ADDRESS')
+GMAIL_APP_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
+RECIPIENTS = tuple(_.strip() for _ in os.environ.get('GARAGE_RECIPIENTS').split(','))
 
-def run():
-  vesync_email = os.environ.get('VESYNC_EMAIL')
-  vesync_password = os.environ.get('VESYNC_PASSWORD')
-  sender = os.environ.get('GMAIL_ADDRESS')
-  gmail_app_password = os.environ.get('GMAIL_APP_PASSWORD')
-  recipients = tuple(_.strip() for _ in os.environ.get('GARAGE_RECIPIENTS').split(','))
-  
-  manager = VeSync(vesync_email, vesync_password)
+def get_outlet_details():
+  manager = VeSync(VESYNC_EMAIL, VESYNC_PASSWORD)
   manager.login()
   manager.update()
   outlet = next(_ for _ in manager.outlets if _.device_name == DEVICE_NAME)
@@ -26,14 +24,29 @@ def run():
     outlet.display()
   
   details = stringio.getvalue()
+
+  return details
+
+def send_email(subject, text):
   message = MIMEText(details)
-  message['Subject'] = f'{DEVICE_NAME} status'
-  message['From'] = sender
-  message['To'] = ', '.join(recipients)
+  message['Subject'] = f'{DEVICE_NAME} {subject}'
+  message['From'] = SENDER
+  message['To'] = ', '.join(RECIPIENTS)
 
   with SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:
-    smtp.login(sender, gmail_app_password)
-    smtp.sendmail(sender, recipients, message.as_string())
-    
+    smtp.login(SENDER, GMAIL_APP_PASSWORD)
+    smtp.sendmail(SENDER, RECIPIENTS, message.as_string())
+
+def run():
+  arg = sys.argv[-1]
+  details = get_outlet_details()
+  print(details)
+
+  if arg == 'report':
+    send_email(f'{DEVICE_NAME} status', details)
+
+  elif arg == 'alarm' and 'off' in details:
+    send_email('Garage alarm', details)
+
 if __name__ == "__main__":
   run()
